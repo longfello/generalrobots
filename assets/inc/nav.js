2d26cef4ac847d7a48fe4c01d0f88c0b3f568ea8
Konstantin Kashirin <konstantin@kashirin.eu>
2021-10-04 10:52:06 -0400

    Add JavaScript navbar & footer

---
+++ assets/inc/nav.js
@@ -0,0 +1,27 @@
+document.write(`
+
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light border-bottom">
+  <div class="container">
+    <a class="navbar-brand" href="/">
+      <img src="./favicons/favicon-32x32.png" alt="" width="24" height="24" class="d-inline-block align-text-top me-2">
+      General Robots
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="d-flex justify-content-center">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <!--<a class="nav-link active" aria-current="page" href="#">Mission</a>-->
+        <a class="nav-link" href="/mission">About</a>
+        <a class="nav-link" href="/products">Product</a>
+        <a class="nav-link" href="/careers">Careers</a>
+        <a class="nav-link" href="/support">Support</a>
+      </div>
+      </div>
+    </div>
+  </div>
+</nav>
+</script>
+
+`);
